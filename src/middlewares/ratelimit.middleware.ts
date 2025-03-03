84b35d7ffcddc5133037ca8c566efc1b7f9f692c
Kumar Sagar <sagar@lifeguru.app>
2025-03-03 18:29:46 +0530

    fix

--- src/middlewares/ratelimit.middleware.ts
+++ src/middlewares/ratelimit.middleware.ts
@@ -2,7 +2,7 @@ import rateLimit from 'express-rate-limit';
 import { env } from '../config/env.config';
 
 export const rateLimiter = rateLimit({
-  windowMs: env.rateLimit.windowMs,
-  max: env.rateLimit.max,
+  // windowMs: env.rateLimit.windowMs,
+  // max: env.rateLimit.max,
   message: 'Too many requests from this IP, please try again later.',
 });
